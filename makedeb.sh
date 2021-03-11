e85f9918b074d57f6a336a8217a6ed5a71f4bfe7
Hunter Wittenborn <git@hunterwittenborn.me>
2021-03-11 04:30:27 -0600

    Add License

--- makedeb.sh
+++ makedeb.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 ################################################
-##      Copyright Hunter Wittenborn 2020      ##
+##  Copyright Hunter Wittenborn 2020          ##
 ##                                            ##
 ##  PKGBUILD, makepkg and related assets are  ##
-##   properties of their respective owners.   ##
+##  properties of their respective owners.    ##
+##                                            ##
+##  This program is free software; you can    ##
+##  redistribute it and/or modify it under    ##
+##  the terms of the GNU General Public       ##
+##  License as published by the Free Software ##
+##  Foundation; either version 3 of the       ##
+##  License, or (at your option) any later    ##
+##  version.                                  ##
 ################################################
+
+##  
 
 
 #################
