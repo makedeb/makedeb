e9b9ef0d61e8a1dcee5cd7718143ef73ade7c3b0
Hunter Wittenborn <git@hunterwittenborn.me>
2021-03-12 05:14:21 -0600

    Add/Modify Features
    - Added basic help menu accessible with "--help"
    - Added skip download option
    - Added option to auto install after building
    - Added support for all checksums listed on the PKGBUILD wiki page linked in README.md

--- makedeb.sh
+++ makedeb.sh
@@ -13,6 +13,14 @@
 ################################################
 
 
+####################
+## DEFAULT VALUES ##
+####################
+
+DOWNLOAD='TRUE'
+INSTALL='FALSE'
+
+
 #################
 ##  FUNCTIONS  ##
 #################
@@ -24,20 +32,38 @@ root_check() {
     fi
     }
 
+help() {
+echo "makedeb v0.1.1"
+echo "Usage: makedeb [options]"
+echo ""
+echo "makedeb takes PKGBUILD files and compiles APT-installable archives."
+echo ""
+echo "Options:"
+echo "  --help - bring up this help menu"
+echo "  --install - automatically install after building"
+echo "  --skip-download - skip downloading of files"
+echo ""
+echo "A config file is located in '~/.config/makedeb/config'."
+echo "Options for this file:"
+echo "    MAINTAINER - set a maintainer for built packages"
+echo ""
+echo "Report bugs at https://github.com/hwittenborn/makedeb"
+}
+
 setup() {
   DIR="$(echo ${PWD})"
   ## DEFAULT VALUES FOR SOME VARIABLES ##
   pkgdir="${DIR}/pkg"
   srcdir="${DIR}/src"
-  
+
   ## CLEANUP TASKS ##
-  rm ${DIR}/makedeb.log &> /dev/null
-  rm ${pkgdir} -R &> /dev/null
+  rm "${DIR}"/makedeb.log &> /dev/null
+  rm "${pkgdir}" -R &> /dev/null
   }
 
 import_pkgbuild() {
   find_pkgbuild=$(find PKGBUILD &> /dev/null; echo ${?})
-  
+
   if [[ ${find_pkgbuild} == "0" ]]; then
     source PKGBUILD
   else
@@ -51,29 +77,29 @@ sanity_check() {
   for check in pkgname pkgver pkgrel pkgdesc arch source; do
     if [[ "(eval \${$check})" == "" ]]; then
       echo "[!] '${check}' is not set in the PKGBUILD"
-      if [[ ${check} == "source" ]]; then 
+      if [[ ${check} == "source" ]]; then
         exit 1
         fi
       fi
   done
   }
-    
+
 config_setup() {
   find ~/.config/makedeb/config &> /dev/null
   if [[ "$?" != "0" ]]; then
     mkdir -p ~/.config/makedeb/ &> /dev/null
     touch ~/.config/makedeb/config
   fi
-  
+
   . ~/.config/makedeb/config
   }
 
 config_import() {
   source ~/.config/makedeb/config
   }
-  
+
 pkgsetup() {
-  rm -r ${pkgdir}
+  rm -r "${pkgdir}" &> /dev/null
   mkdir -p "${pkgdir}"/DEBIAN/
   touch "${pkgdir}"/DEBIAN/control
   }
@@ -83,7 +109,7 @@ export_control() {
     echo "${1} ${2}" >> "${pkgdir}"/DEBIAN/control
   fi
   }
-  
+
 convert_version() {
   if [[ ${epoch} == "" ]]; then
     export pkgver="${pkgver}-${pkgrel}"
@@ -97,7 +123,7 @@ convert_arch() {
     export arch="amd64"
   fi
   }
-  
+
 convert_dependencies() {
   if [[ ${depends} != "" ]]; then
     echo "Depends:" >> "${pkgdir}"/DEBIAN/control
@@ -109,45 +135,65 @@ convert_dependencies() {
   }
 
 pull_sources() {
-    echo ${1} | grep "http" &> /dev/null
-    if [[ ${?} == "0" ]]; then
-      echo "[#] Pulling $(basename ${1}) ..."
-      wget ${1} -q --show-progress
-    else
-      echo "[#] Copying ${1} to source directory ..."
-      cp ${DIR}/${1} "${srcdir}"
-    fi
-  }
-  
-source_files() {
-  for source_check in ${source[@]}; do
-    (find "${srcdir}/$(basename ${source_check})" &> /dev/null)
-    if [[ ${?} != "0" ]]; then
-    pull_sources ${source_check}
-    fi
-    done
-    }
-    
+if [[ ${DOWNLOAD} == "TRUE" ]]; then
+  for pkg in ${source[@]}; do
 
-count_sums() {
-  sum_count=$(($(echo ${sha256sums} | wc -w) - 1))
-  }
+    echo "${pkg}" | grep "::" &> /dev/null
+    if [[ ${?} == 0 ]]; then
 
-verify_sources() {
-  sum_current="0"
-  while [[ ${sum_current} -le ${sum_count} ]]; do
-    echo "[#] Checking integrety of $(basename ${source[${sum_current}]}) ..."
-    current_sum=$(sha256sum "${srcdir}"/$(basename "${source[${sum_current}]}") | awk '{print $1}')
-    if [[ ${current_sum} != "${sha256sums[${sum_current}]}" ]]; then
-      echo "[!] "$(basename "${source[${sum_current}]}")" failed the integrety check."
-      echo "[!] Try deleting the file from '${srcdir}/$(basename ${source[${sum_current}]})' and run makedeb again"
-      exit 1
+      ## OBTAIN TWO STRINGS FROM SOURCE ##
+      string1=$(echo ${pkg} | awk -F:: '{print $1}')
+      string2=$(echo ${pkg} | awk -F:: '{print $2}')
+
+      ## CHECK FOR URL IN SECOND STRING ##
+      echo "${string2}" | grep "http" &> /dev/null
+      if [[ ${?} == 0 ]]; then
+      echo "[#] Downloading '$(basename $string2)' to '${string1}' ..."
+        wget -q --show-progress ${string2}
+        echo "[#] Moving '$(basename $string2)' to '${string1}' ..."
+        mv $(basename $string2) ${string1}
+      else
+        echo "[#] Copying "'${DIR}"/"${string2}'" to "'${string1}'" ..."
+        cp "'${DIR}'"/"'${string2}'" "'${string1}'"
+      fi
     else
-      echo "[#] Passed."
-    sum_current=$((${sum_current} + 1))
+
+      ## CHECK FOR URL IN SINGLE STRING ##
+      echo "${pkg}" | grep "http" &> /dev/null
+      if [[ ${?} == 0 ]]; then
+      echo "[#] Downloading '$(basename $pkg)' ..."
+        wget -q --show-progress ${pkg}
+      else
+        echo "[#] Copying '${pkg}' to source directory"
+        cp "${DIR}"/"${pkg}" .
+      fi
     fi
   done
+else
+  echo "[#] Skipping download ..."
+  fi
   }
+
+hashsum_check() {
+  for do_hash in md5sums sha256sums sha1sums sha224sums sha384sums sha512sums b2sums; do
+    ## CHECK IF HASHES ARE SET FOR EACH TYPE ##
+    if [[ ${!do_hash} != "" ]]; then
+      source_file="0"
+      source_total=$(( $(echo ${source[@]} | wc -w) -1 ))
+      recorded_hash=${!do_hash[$source_file]}
+      current_hash=$( ${do_hash::-1} $(basename ${source[$source_file]}) | awk '{print $1}')
+
+      if [[ ${!do_hash} == "SKIP" ]]; then
+        echo "[#] Skipping ${do_hash::-1} check on '$(basename ${source[$source_file]})'."
+      elif [[ ${current_hash} != ${recorded_hash} ]]; then
+        echo "[!] '$(basename ${source[$source_file]})' failed the integrity check on ${do_hash}."
+        exit 1
+      else
+        echo "[#] '$(basename ${source[$source_file]})' passed the integrity check on ${do_hash}."
+      fi
+    fi
+  done
+}
 
 run_functions() {
     type ${1} &> /dev/null
@@ -162,7 +208,7 @@ build_package() {
   dpkg -b "${pkgdir}" >> /dev/null
   dpkg-name "${pkgdir}.deb" >> /dev/null
   }
-  
+
 check_build() {
   find "${srcdir}/.pkg_built" &> /dev/null
   if [[ ${?} == "0" ]]; then
@@ -172,15 +218,25 @@ check_build() {
     exit 0
   fi
   }
+
 ####################
 ##  START SCRIPT  ##
 ####################
 
+config_setup
+while true; do
+  case "${1}" in
+  --help)                                 help; exit 0 ;;
+  --install)                              INSTALL="TRUE" ;;
+  --skip-download)                        DOWNLOAD="FALSE" ;;
+  "")                                     break ;;
+  esac
+  shift
+  done
 root_check
 setup
 import_pkgbuild
 sanity_check
-config_setup
 check_build
 
 echo "[#] Preparing ..."
@@ -193,15 +249,16 @@ export_control "Source:" "${source}"
 export_control "Version:" "${pkgver}"
 export_control "Architecture:" "${arch}"
 export_control "Maintainer:" "${MAINTAINER}"
-convert_dependencies
+# convert_dependencies
 echo "" >> "${pkgdir}"/DEBIAN/control
 
 ## PULL AND VERIFY SOURCES ##
 mkdir -p "${srcdir}"
 cd "${srcdir}"
 
-source_files
-verify_sources
+pull_sources
+hashsum_check
+exit 0
 
 ## RUN PREPARE, BUILD, and PACKAGE FUNCTIONS ##
 cd "${srcdir}"
@@ -211,5 +268,9 @@ cd "${DIR}"
 run_functions package
 touch "${srcdir}/.pkg_built"
 
-## BUILD PACKAGE ##
+## BUILD AND INSTALL PACKAGE ##
 build_package
+if [[ ${INSTALL} == "TRUE" ]]; then
+  echo "[#] Installing ${pkgname}_${pkgver}_${arch}.deb"
+  sudo apt install "${pkgdir}/${pkgname}_${pkgver}_${arch}.deb"
+fi
