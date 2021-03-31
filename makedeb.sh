9d46dc9f429c8d6693c104eef1ed36f3b5bdcbcd
Hunter Wittenborn <git@hunterwittenborn.me>
2021-03-30 22:33:36 -0500

    Update copyright message

--- makedeb.sh
+++ makedeb.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-
-################################################
-##  Copyright Hunter Wittenborn 2021          ##
-##                                            ##
-##  PKGBUILD, makepkg and related assets are  ##
-##  properties of their respective owners.    ##
-##                                            ##
-##  This program is free software; you can    ##
-##  redistribute it and/or modify it under    ##
-##  the terms of the GNU General Public       ##
-##  License v3.0                              ##
-################################################
-
+# Copyright 2020 Hunter Wittenborn <git@hunterwittenborn.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# PKGBUILD, makepkg and related assets are properties of their
+# respective owners.
 
 ####################
 ## DEFAULT VALUES ##
