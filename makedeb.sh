6c7038a2cce17e3eaa7724f9e885a18c50204ceb
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-04-26 10:21:18 -0500

    Fix syntax error in makedeb.sh
    Added 'fi' at line 76 to fix syntax error in if statement

--- makedeb.sh
+++ makedeb.sh
@@ -73,6 +73,7 @@ root_check() {
   if [[ "$(whoami)" == "root" ]]; then
     echo "Running makedeb as root is not allowed as it can cause irreversable damage to your system."
     exit 1
+  fi
 }
 
 pkgsetup() {
