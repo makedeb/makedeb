db334b72e4bbb7203d9cf7c523ee113ca2dde59c
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-15 05:06:02 -0500

    Fixed issue with wget command in aur_pkgbuild_select.sh

--- scripts/aur_pkgbuild_select.sh
+++ scripts/aur_pkgbuild_select.sh
@@ -3,9 +3,9 @@ set -e
 
 rm src/PKGBUILD
 if [[ "${release_type}" == "stable" ]]; then
-  wget "https://github.com/hwittenborn/makedeb/raw/alpha/src/PKGBUILDs/PKGBUILD_AUR_STABLE" -O src/PKGBUILD
+  wget "https://github.com/hwittenborn/makedeb/raw/alpha/src/PKGBUILDs/PKGBUILD_AUR_STABLE" -O */src/PKGBUILD
 
 elif [[ "${release_type}" == "alpha" ]]; then
-  wget "https://github.com/hwittenborn/makedeb/raw/alpha/src/PKGBUILDs/PKGBUILD_AUR_ALPHA" -O src/PKGBUILD
+  wget "https://github.com/hwittenborn/makedeb/raw/alpha/src/PKGBUILDs/PKGBUILD_AUR_ALPHA" -O */src/PKGBUILD
 
 fi
