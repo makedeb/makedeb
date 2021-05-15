19b620ae54ca51e8e8f6d7ae2f5cb519b0b36f4f
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-15 05:10:22 -0500

    Fixed issue with rm command in aur_pkgbuild_select.sh

--- scripts/aur_pkgbuild_select.sh
+++ scripts/aur_pkgbuild_select.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-rm src/PKGBUILD
+rm */src/PKGBUILD
 if [[ "${release_type}" == "stable" ]]; then
   wget "https://github.com/hwittenborn/makedeb/raw/alpha/src/PKGBUILDs/PKGBUILD_AUR_STABLE" -O */src/PKGBUILD
 
