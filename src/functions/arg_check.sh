63516238855e091c48dc4c89253db618c89dc8c7
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-25 03:09:07 -0500

    Added '--printsrcinfo' option; changed '-I' to '-i'

--- src/functions/arg_check.sh
+++ src/functions/arg_check.sh
@@ -3,13 +3,18 @@ arg_check() {
     case "${1}" in
       -B | --prebuilt)     PREBUILT="TRUE" ;;
       -C | --convert)      package_convert="true" ;;
-      -F | --file | -p)    FILE=${2}; shift;;
+      -F | --file | -p)    FILE="${2}"; shift;;
       -h | --help)         help; exit 0 ;;
-      -I | --install)      INSTALL="TRUE" ;;
+      -i | --install)      INSTALL="TRUE" ;;
       -P | --pkgname)      prebuilt_pkgname="${2}"; shift 1 ;;
+
+      --printsrcinfo)      export makepkg_printsrcinfo="true" ;;
+
       -*)                  echo "Unknown option '${1}'"; exit 1 ;;
       "")                  break ;;
     esac
     shift 1
     done
+
+    [[ "${makepkg_printsrcinfo}" == "true" ]] && { makepkg --printsrcinfo -p "${FILE:-PKGBUILD}"; exit ${?}; }
 }
--- src/functions/help.sh
+++ src/functions/help.sh
@@ -5,14 +5,16 @@ help() {
   echo "makedeb takes PKGBUILD files and compiles APT-installable archives."
   echo
   echo "Options:"
-  echo "  Items must be space-separated, i.e. '-I -F'"
+  echo "  Items must be space-separated, i.e. '-i -B'"
   echo
   echo "  -B, --prebuilt - make a Debian package from a prebuilt Arch package; Requires a build file to be present"
   echo "  -C, --convert - convert dependencies and architectures"
   echo "  -F, --file, -p - specify a build file other than 'PKGBUILD'"
   echo "  -h, --help - bring up this help menu"
-  echo "  -I, --install - automatically install after building"
+  echo "  -i, --install - automatically install after building"
   echo "  -P, --pkgname - specify the pkgname for prebuilt packages (--prebuilt) when 'pkgname' is an array"
   echo
+  echo "The following options can be passed to makepkg:"
+  echo "  --printsrcinfo - print a generated .SRCINFO file and exit"
   echo "Report bugs at https://github.com/hwittenborn/makedeb"
 }
