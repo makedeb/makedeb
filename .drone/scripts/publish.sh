2ac92d807517bc0783879a0ec07853f0fe5e6560
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-06-18 02:54:28 -0500

    More Drone CI testing; added 'git' as optdepend for makedeb

--- .drone/scripts/publish.sh
+++ .drone/scripts/publish.sh
@@ -15,13 +15,14 @@ cd src
 component_name="$(cat PKGBUILD | grep 'pkgname=' | sed 's|pkgname=||')"
 
 # Check for build debs
-deb_packages=$(find *.deb 2> /dev/null | wc -w)
+deb_packages=$(find *.deb 2> /dev/null)
+deb_packages_count=$(echo "${deb_packages}" | wc -w)
 
-if [[ "${deb_packages}" == "0" ]]; then
+if [[ "${deb_packages_count}" == "0" ]]; then
     echo "ERROR: There doesn't appear to be any packages present."
     exit 1
 
-elif [[ "${deb_packages}" -gt "1" ]]; then
+elif [[ "${deb_packages_count}" -gt "1" ]]; then
     echo "ERROR: More than one package is present."
     exit 1
 fi
