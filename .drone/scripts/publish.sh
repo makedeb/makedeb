f9e692a8c91b37b2715b7c268e3edd9da4bf6359
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-06-18 02:36:03 -0500

    More Drone CI testing

--- .drone/scripts/publish.sh
+++ .drone/scripts/publish.sh
@@ -6,7 +6,7 @@ apt update
 apt install wget -y
 
 wget -qO - "https://${proget_server}/debian-feeds/makedeb.pub" | gpg --dearmor | tee /usr/share/keyrings/makedeb-archive-keyring.gpg &> /dev/null
-echo "deb [signed-by=/usr/share/keyrings/makedeb-archive-keyring.gpg arch=all] https://${proget_server} makedeb main"
+echo "deb [signed-by=/usr/share/keyrings/makedeb-archive-keyring.gpg arch=all] https://${proget_server} makedeb main" | tee /etc/apt/sources.list.d/makedeb.list
 
 apt update
 apt install makedeb-alpha -y
