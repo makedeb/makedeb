1cdcb73b929f83e6831ac3a76ead4ca85ff38a82
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-06-18 02:42:22 -0500

    More Drone CI testing

--- .drone/scripts/build.sh
+++ .drone/scripts/build.sh
@@ -3,7 +3,7 @@
 # Install needed packages
 echo "Installing needed packages..."
 apt update
-apt install wget sudo gpg -y
+apt install wget sudo gpg git -y
 
 wget -qO - "https://${proget_server}/debian-feeds/makedeb.pub" | gpg --dearmor | tee /usr/share/keyrings/makedeb-archive-keyring.gpg &> /dev/null
 echo "deb [signed-by=/usr/share/keyrings/makedeb-archive-keyring.gpg arch=all] https://${proget_server} makedeb main" | tee /etc/apt/sources.list.d/makedeb.list
