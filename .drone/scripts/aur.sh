5f6c32cea58134cdf3f1ffca8af2326d910446bc
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-06-18 06:03:23 -0500

    Drone CI testing

--- .drone/scripts/aur.sh
+++ .drone/scripts/aur.sh
@@ -7,7 +7,7 @@ apt update
 apt install ssh git gpg -y
 
 wget -qO - "https://${proget_server}/debian-feeds/makedeb.pub" | gpg --dearmor | tee /usr/share/keyrings/makedeb-archive-keyring.gpg &> /dev/null
-echo "deb [signed-by=/usr/share/keyrings/makedeb-archive-keyring.gpg arch=all] https://${proget_server} makedeb main"
+echo "deb [signed-by=/usr/share/keyrings/makedeb-archive-keyring.gpg arch=all] https://${proget_server} makedeb main" | tee /etc/apt/sources.list.d/makedeb.list
 
 apt update
 apt install makepkg -y
