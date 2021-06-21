8bf4ac506c3094e88f3c82a1e7a3585a00686602
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-06-21 06:47:17 -0500

    Updated conflicts() for built packages

--- .drone/scripts/build.sh
+++ .drone/scripts/build.sh
@@ -10,6 +10,12 @@ chmod 777 * -R
 sed -i "s|(git release)|(${release_type} release)|" src/PKGBUILD
 sed -i "s|pkgname=.*|pkgname=${package_name}|" src/PKGBUILD
 
+if [[ "${release_type}" == "stable" ]]; then
+  echo 'conflicts=("makedeb-alpha")' | tee -a src/PKGBUILD
+elif [[ "${release_type}" == "alpha" ]]; then
+  echo 'conflicts=("makedeb")' | tee -a src/PKGBUILD
+fi
+
 # Build makedeb
 cd src
 sudo -u user './makedeb.sh'
