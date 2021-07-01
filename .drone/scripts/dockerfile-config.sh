379c84cabf4fb36304eb4c4bc3efee15eed4ce86
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-07-01 17:33:13 -0500

    Fixed issue with makedeb-beta Docker image

--- .drone/scripts/dockerfile-config.sh
+++ .drone/scripts/dockerfile-config.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ "${release_type}" == "alpha" ]]; then
+if [[ "${release_type}" == "beta" ]]; then
+    sed -i 's|package=.*|package="makedeb-beta"|' docker/Dockerfile
+elif [[ "${release_type}" == "alpha" ]]; then
     sed -i 's|package=.*|package="makedeb-alpha"|' docker/Dockerfile
 fi
