04c26a4b775592dc1ab7341582bbb4de10ef5bb8
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-07-02 15:12:20 -0500

    Moved arg_check() and help() to different parts based on distribution

--- src/functions/arg_check.sh
+++ src/functions/arg_check.sh
@@ -1,4 +1,6 @@
 arg_check() {
+    # Convert grouped short options into single-character short options
+    # (i.e. -ae ==> -a -e)
     for i in ${@}; do
         if [[ "$(echo "${i}" | grep -E '^\-[^-][a-z]')" != "" ]]; then
             number="2"
@@ -16,25 +18,42 @@ arg_check() {
         fi
     done
 
-
+    # Actual argument check
     for i in ${argument_list}; do
-        case "${i}" in
-            -C | --convert)            export package_convert="true" ;;
-            -d | --nodeps)             export skip_dependency_checks="true"; export makepkg_options+=" --nodeps" ;;
-            -F | -p | --file)          export FILE="${2}"; shift;;
-            -h | --help)               help; exit 0 ;;
-            -i | --install)            export INSTALL="TRUE" ;;
-            -s | --syncdeps)           export install_dependencies="true"; export makepkg_options+=" --syncdeps" ;;
-            -v | --distro-packages)    export distro_packages="true" ;;
-            --dur-check)               export dur_check="true" ;;
+        if [[ "${target_os}" == "debian" ]]; then
+            case "${i}" in
+                -d | --nodeps)             export skip_dependency_checks="true"; export makepkg_options+=" --nodeps" ;;
+                -F | -p | --file)          export FILE="${2}"; shift;;
+                -h | --help)               help; exit 0 ;;
+                -i | --install)            export INSTALL="TRUE" ;;
+                -s | --syncdeps)           export install_dependencies="true"; export makepkg_options+=" --syncdeps" ;;
+                -v | --distro-packages)    export distro_packages="true" ;;
+                --dur-check)               export dur_check="true" ;;
 
-            --printsrcinfo)            export makepkg_printsrcinfo="true" ;;
-            --skippgpcheck)            export makepkg_options+=" --skippgpcheck" ;;
+                --printsrcinfo)            export makepkg_printsrcinfo="true" ;;
+                --skippgpcheck)            export makepkg_options+=" --skippgpcheck" ;;
 
-            -*)                        error "Unknown option '${1}'"; exit 1 ;;
-            "")                        break ;;
-        esac
-        shift 1 || true
+                -*)                        error "Unknown option '${1}'"; exit 1 ;;
+                "")                        break ;;
+            esac
+            shift 1 || true
+
+        elif [[ "${target_os}" == "arch" ]]; then
+            case "${i}" in
+                -F | -p | --file)          export FILE="${2}"; shift;;
+                -h | --help)               help; exit 0 ;;
+                --dur-check)               export dur_check="true" ;;
+
+                -d | --nodeps)             export skip_dependency_checks="true"; export makepkg_options+=" --nodeps" ;;
+                -s | --syncdeps)           export install_dependencies="true"; export makepkg_options+=" --syncdeps" ;;
+                --printsrcinfo)            export makepkg_printsrcinfo="true" ;;
+                --skippgpcheck)            export makepkg_options+=" --skippgpcheck" ;;
+
+                -*)                        error "Unknown option '${1}'"; exit 1 ;;
+                "")                        break ;;
+            esac
+            shift 1 || true
+        fi
     done
 
     if [[ "${makepkg_printsrcinfo}" == "true" ]]; then makepkg --printsrcinfo -p "${FILE:-PKGBUILD}"; exit ${?}; fi
--- src/functions/help.sh
+++ src/functions/help.sh
@@ -1,37 +1,42 @@
 help() {
-    # makedeb section
-    echo "makedeb (${makedeb_package_version}) - Create Debian archives from PKGBUILDs"
-    echo "Usage: makedeb [options]"
-    echo
-    echo "makedeb takes PKGBUILD files and builds archives installable with APT."
-    echo
-    echo "Options:"
-    echo "  -C, --convert - Attempt to convert and modify Arch Linux dependencies in build file to align with Debian"
     if [[ "${target_os}" == "debian" ]]; then
-    echo "  -d, --nodeps - Skip all dependency checks"
-    fi
-    echo "  -F, --file, -p - specify a build file other than 'PKGBUILD'"
-    echo "  -h, --help - bring up this help menu"
-    if [[ "${target_os}" == "debian" ]]; then
-    echo "  -i, --install - automatically install after building"
-    fi
-    if [[ "${target_os}" == "debian" ]]; then
+        echo "makedeb (${makedeb_package_version}) - Create Debian archives from PKGBUILDs"
+        echo "Usage: makedeb [options]"
+        echo
+        echo "makedeb takes PKGBUILD files and builds archives installable with APT."
+        echo
+        echo "Options:"
+        echo "  -d, --nodeps - Skip all dependency checks"
+        echo "  -F, --file, -p - specify a build file other than 'PKGBUILD'"
+        echo "  -h, --help - bring up this help menu"
+        echo "  -H, --field - append the resulting control file with custom fields"
+        echo "  -i, --install - automatically install after building"
         echo "  -v, --distro-packages - source package relationships from distro-specific variables when they exist"
+        echo "  -s, --syncdeps - install missing dependencies with APT"
+        echo
+        echo "The following options can be passed to makepkg:"
+        echo "  --printsrcinfo - print a generated .SRCINFO file and exit"
+        echo "  --skippgpcheck - Do not verify source files against PGP signatures"
+        echo
+        echo "Report bugs at https://github.com/hwittenborn/makedeb"
+
+    elif [[ "${target_os}" == "arch" ]]; then
+        echo "makedeb (${makedeb_package_version}) - Create Debian archives from PKGBUILDs"
+        echo "Usage: makedeb [options]"
+        echo
+        echo "makedeb takes PKGBUILD files and builds archives installable with APT."
+        echo
+        echo "Options:"
+        echo "  -F, --file, -p - specify a build file other than 'PKGBUILD'"
+        echo "  -h, --help - bring up this help menu"
+        echo "  -H, --field - append the resulting control file with custom fields"
+        echo
+        echo "The following options can be passed to makepkg:"
+        echo "  -d, --nodeps - Skip all dependency checks"
+        echo "  -s, --syncdeps - install missing dependencies with pacman"
+        echo "  --printsrcinfo - print a generated .SRCINFO file and exit"
+        echo "  --skippgpcheck - Do not verify source files against PGP signatures"
+        echo
+        echo "Report bugs at https://github.com/hwittenborn/makedeb"
     fi
-    if [[ "${target_os}" == "debian" ]]; then
-    echo "  -s, --syncdeps - install missing dependencies with APT"
-    fi
-    echo
-    # makepkg section
-    echo "The following options can be passed to makepkg:"
-    if [[ "${target_os}" == "arch" ]]; then
-    echo "  -d, --nodeps - Skip all dependency checks"
-    fi
-    if [[ "${target_os}" == "arch" ]]; then
-    echo "  -s, --syncdeps - install missing dependencies with pacman"
-    fi
-    echo "  --printsrcinfo - print a generated .SRCINFO file and exit"
-    echo "  --skippgpcheck - Do not verify source files against PGP signatures"
-    echo
-    echo "Report bugs at https://github.com/hwittenborn/makedeb"
 }
