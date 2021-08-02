04d2c54ac49aec812c87881d7c3c98c56276cab3
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-08-02 04:12:42 -0500

    Fixed issue with '--version' command

--- src/functions/version_info.sh
+++ src/functions/version_info.sh
@@ -1,5 +1,5 @@
 version_info() {
-    makepkg_package_version="$(makepkg --version | awk NR=="1" | awk '{print $3}')"
+    makepkg_package_version="$("${makepkg_package_name}" --version | awk NR=="1" | grep -o '([^)]*' | sed 's|(||')"
 
     echo "makedeb (${makedeb_package_version}) - ${makedeb_release_type^} Release"
     echo "makepkg (${makepkg_package_version})"
