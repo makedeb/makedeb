97f800290091cbc261fc99c4fa8cfb0dc1d8cbb2
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-08-08 00:44:15 -0500

    Fixed issue with placement of 'url' PKGBUILD variable in control files
    Solves https://github.com/makedeb/makedeb/issues/24
    
    Also reordered the output of control file so that pkgname, 
    pkgver-pkgrel, and pkgdesc are at the top, followed by arch, license, 
    maintainer, and then the url.

--- src/functions/generate_control.sh
+++ src/functions/generate_control.sh
@@ -6,12 +6,12 @@ generate_control() {
 	local maintainer="$(cat ../../${FILE} | grep '\# Maintainer\:' | sed 's/# Maintainer: //' | xargs | sed 's|>|>,|g' | rev | sed 's|,||' | rev)"
 
     export_control "Package:" "${pkgname}"
+	export_control "Version:" "${built_archive_version}"
     export_control "Description:" "${pkgdesc}"
-    export_control "Source:" "${url}"
-    export_control "Version:" "${built_archive_version}"
-    export_control "Architecture:" "${makedeb_arch}"
-    export_control "License:" "${license}"
-    export_control "Maintainer:" "${maintainer}"
+	export_control "Architecture:" "${makedeb_arch}"
+	export_control "License:" "${license}"
+	export_control "Maintainer:" "${maintainer}"
+    export_control "Homepage:" "${url}"
 
     eval export_control "Depends:" "${depends[@]@Q}"
 	eval export_control "Recommends:" "${recommends[@]@Q}"
