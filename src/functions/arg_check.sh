418c42d301784249080098673b964391096d20eb
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-09-23 21:24:31 -0500

    Modified arguments to hyphenate between words
    Also kept previous arguments as valid to prevent any breaking changes :)

--- src/functions/arg_check.sh
+++ src/functions/arg_check.sh
@@ -5,27 +5,25 @@ arg_check() {
   if [[ "${target_os}" == "debian" ]]; then
     while [[ "${1}" != "" ]]; do
       case "${1}" in
-        -A | --ignore-arch)        declare -g makepkg_options+=("--ignorearch") ;;
-        -d | --nodeps)             declare -g skip_dependency_checks="true"; declare makepkg_options+=("--nodeps") ;;
-        -F | -p | --file)          declare -g FILE="${2}"; shift ;;
-        -h | --help)               help; exit 0 ;;
-        -H | --field)              export  extra_control_fields+=("${2}"); shift ;;
-        -i | --install)            declare -g INSTALL="TRUE" ;;
-        -Q | --no-fields)          warning "'${1}' has been deprecated, and should not be used." ;;
-        -r | --rmdeps)             declare -g remove_dependencies="true"; declare makepkg_options+=("--rmdeps") ;;
-        -s | --syncdeps)           declare -g install_dependencies="true"; declare makepkg_options+=("--syncdeps") ;;
-        -v | --distro-packages)    warning "'${1}' has been deprecated, and should not be used." ;;
-        -V | --version)            version_info; exit 0 ;;
-        --dur-check)               declare -g dur_check="true" ;;
-        --print-control)           declare -g print_control=1 ;;
-        --verbose)                 set -x ;;
-
-        -g | --geninteg)           declare -g makepkg_geninteg="true" ;;
-        --printsrcinfo)            declare -g makepkg_printsrcinfo="true" ;;
-        --skippgpcheck)            declare -g makepkg_options+=("--skippgpcheck") ;;
-
-        -*)                        error "Unknown option '${1}'"; exit 1 ;;
-        "")                        break ;;
+        -A | --ignore-arch | --ignorearch) declare -g makepkg_options+=("--ignorearch") ;;
+        -d | --no-deps | --nodeps)         declare -g skip_dependency_checks="true"; declare makepkg_options+=("--nodeps") ;;
+        -F | -p | --file)                  declare -g FILE="${2}"; shift ;;
+        -g | --gen-integ | --geninteg)     declare -g makepkg_geninteg="true" ;;
+        -h | --help)                       help; exit 0 ;;
+        -H | --field)                      export  extra_control_fields+=("${2}"); shift ;;
+        -i | --install)                    declare -g INSTALL="TRUE" ;;
+        -Q | --no-fields)                  warning "'${1}' has been deprecated, and should not be used." ;;
+        -r | --rm-deps | --rmdeps)         declare -g remove_dependencies="true"; declare makepkg_options+=("--rmdeps") ;;
+        -s | --sync-deps | --syncdeps)     declare -g install_dependencies="true"; declare makepkg_options+=("--syncdeps") ;;
+        -v | --distro-packages)            warning "'${1}' has been deprecated, and should not be used." ;;
+        -V | --version)                    version_info; exit 0 ;;
+        --dur-check)                       declare -g dur_check="true" ;;
+        --print-control)                   declare -g print_control=1 ;;
+        --print-srcinfo | --printsrcinfo)  declare -g makepkg_printsrcinfo="true" ;;
+        --skip-pgp-check | --skippgpcheck) declare -g makepkg_options+=("--skippgpcheck") ;;
+        --verbose)                         set -x ;;
+        -*)                                error "Unknown option '${1}'"; exit 1 ;;
+        "")                                break ;;
       esac
       shift 1 || true
     done
@@ -33,26 +31,24 @@ arg_check() {
   elif [[ "${target_os}" == "arch" ]]; then
     while [[ "${1}" != "" ]]; do
       case "${1}" in
-        -A | --ignore-arch)        declare -g makepkg_options+=("--ignorearch") ;;
-        -F | -p | --file)          declare -g FILE="${2}"; shift ;;
-        -h | --help)               help; exit 0 ;;
-        -H | --field)              export  extra_control_fields+=("${2}"); shift ;;
-        -Q | --no-fields)          warning "'${1}' has been deprecated, and should not be used." ;;
-        -v | --distro-packages)    warning "'${1}' has been deprecated, and should not be used." ;;
-        -V | --version)            version_info; exit 0 ;;
-        --dur-check)               declare -g dur_check="true" ;;
-        --print-control)           declare -g print_control=1 ;;
-        --verbose)                 set -x ;;
-
-        -d | --nodeps)             declare -g skip_dependency_checks="true"; declare makepkg_options+=("--nodeps") ;;
-        -g | --geninteg)           declare -g makepkg_geninteg="true" ;;
-        -r | --rmdeps)             declare -g remove_dependencies="true"; declare makepkg_options+=("--rmdeps") ;;
-        -s | --syncdeps)           declare -g install_dependencies="true"; declare makepkg_options+=("--syncdeps") ;;
-        --printsrcinfo)            declare -g makepkg_printsrcinfo="true" ;;
-        --skippgpcheck)            declare -g makepkg_options+=("--skippgpcheck") ;;
-
-        -*)                        error "Unknown option '${1}'"; exit 1 ;;
-        "")                        break ;;
+        -A | --ignore-arch | --ignorearch) declare -g makepkg_options+=("--ignorearch") ;;
+        -d | --no-deps | --nodeps)         declare -g skip_dependency_checks="true"; declare makepkg_options+=("--nodeps") ;;
+        -F | -p | --file)                  declare -g FILE="${2}"; shift ;;
+        -g | --gen-integ | --geninteg)     declare -g makepkg_geninteg="true" ;;
+        -h | --help)                       help; exit 0 ;;
+        -H | --field)                      export  extra_control_fields+=("${2}"); shift ;;
+        -Q | --no-fields)                  warning "'${1}' has been deprecated, and should not be used." ;;
+        -r | --rm-deps | --rmdeps)         declare -g remove_dependencies="true"; declare makepkg_options+=("--rmdeps") ;;
+        -s | --sync-deps | --syncdeps)     declare -g install_dependencies="true"; declare makepkg_options+=("--syncdeps") ;;
+        -v | --distro-packages)            warning "'${1}' has been deprecated, and should not be used." ;;
+        -V | --version)                    version_info; exit 0 ;;
+        --dur-check)                       declare -g dur_check="true" ;;
+        --print-control)                   declare -g print_control=1 ;;
+        --print-srcinfo | --printsrcinfo)  declare -g makepkg_printsrcinfo="true" ;;
+        --skip-pgp-check | --skippgpcheck) declare -g makepkg_options+=("--skippgpcheck") ;;
+        --verbose)                         set -x ;;
+        -*)                                error "Unknown option '${1}'"; exit 1 ;;
+        "")                                break ;;
       esac
       shift 1 || true
     done
