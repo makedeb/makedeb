d010f411381163d1f926010aa31b734e66f0a880
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-09-25 19:17:29 -0500

    Added check to make sure system codename is a valid variable name for distro-specific dependencies

--- src/functions/check_distro_dependencies.sh
+++ src/functions/check_distro_dependencies.sh
@@ -3,6 +3,15 @@ check_distro_dependencies() {
         variable_ref="" \
         package_data=""
 
+  # Skip checking of distribution dependencies if the system's codename isn't a
+  # valid variable name.
+  bad_variable_chars="$(echo "${distro_version}" | sed 's|^[a-zA-Z_][a-zA-Z0-9_]*||')"
+
+  if [[ "${bad_variable_chars}" != "" ]]; then
+    return
+  fi
+
+  # Continue with checking of variables.
   for i in depends optdepends conflicts provides replaces makedepends optdepends; do
     eval package_data=("\"\${${distro_version}_${i}[@]}\"")
 
