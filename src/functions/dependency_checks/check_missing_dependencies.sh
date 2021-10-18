05a73f9ea0b9e9c7d39f19f780a9f9e09454e75f
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-10-17 19:59:42 -0500

    Fixed issue with missing_dependencies_no_relations and missing_build_dependencies_no_relations variables used for build dependency removal.
    Also simplified code used to find missing build
    dependencies.

--- src/functions/dependency_checks/check_missing_dependencies.sh
+++ src/functions/dependency_checks/check_missing_dependencies.sh
@@ -1,4 +1,5 @@
 check_missing_dependencies() {
+  # Get list of missing dependencies.
   if [[ "${target_os}" == "debian" ]]; then
     if ! missing_dependencies="$("${makedeb_utils_dir}/missing_apt_dependencies.py" "${predepends[@]}" "${depends[@]}")"; then
       error "There was an error checking build dependencies."
@@ -7,58 +8,40 @@ check_missing_dependencies() {
       error "There was an error checking build dependencies."
       exit 1
     fi
-    
-    for i in 'missing_dependencies' 'missing_build_dependencies'; do
-      if [[ "${!i}" == "" ]]; then
-        unset "${i}"
-      fi
-    done
-    
-    # Create dependency arrays without relationships attached.
-    declare missing_dependencies_no_relations=()
-    declare missing_build_dependencies_no_relations=()
-    
-    for i in 'missing_dependencies' 'missing_build_dependencies'; do
-      declare current_array=()
-      declare var_string="${i}[@]"
-      
-      for j in "${!var_string}"; do
-        declare current_program="$(echo "${j}" | sed 's| (.*||')"
-        declare current_array+=("${current_program}")
-      done
-      
-      create_array "${i}_no_relations" "${current_array[@]}"
-      unset current_array
-    done
-    
+        
   elif [[ "${target_os}" == "arch" ]]; then
     missing_dependencies="$(pacman -T -- "${predepends[@]}" "${depends[@]}")" || true
     missing_build_dependencies="$(pacman -T -- "${makedepends[@]}" "${checkdepends[@]}")" || true
-    
-    for i in 'missing_dependencies' 'missing_build_dependencies'; do
-      if [[ "${!i}" == "" ]]; then
-        unset "${i}"
-      fi
-    done
-    
-    # Create dependency arrays without relationships attached (for when removing
-    # build dependencies).
-    declare missing_dependencies_no_relations=()
-    declare missing_build_dependencies_no_relations=()
-    
-    for i in 'missing_dependencies' 'missing_build_dependencies'; do
-      declare current_array=()
-      declare var_string="${i}[@]"
-            
-      for j in "${!var_string}"; do
-        declare current_program="$(echo "${j}" | sed -E 's/<.*|>.*|=.*//')"
-        declare current_array+=("${current_program}")
-      done
-      
-      create_array "${i}_no_relations" "${current_array[@]}"
-      unset current_array
-    done
   fi
+  
+  # Unset variables if no missing dependencies were found.
+  for i in 'missing_dependencies' 'missing_build_dependencies'; do
+    if [[ "${!i}" == "" ]]; then
+      unset "${i}"
+    fi
+  done
+    
+  # Map output of commands to arrays.
+  mapfile -t missing_dependencies < <(echo -n "${missing_dependencies}")
+  mapfile -t missing_build_dependencies < <(echo -n "${missing_build_dependencies}")
+
+  # Create dependency arrays without relationships attached (for when removing
+  # build dependencies).
+  declare missing_dependencies_no_relations=()
+  declare missing_build_dependencies_no_relations=()
+    
+  for i in 'missing_dependencies' 'missing_build_dependencies'; do
+    declare current_array=()
+    declare var_string="${i}[@]"
+            
+    for j in "${!var_string}"; do
+      declare current_program="$(echo "${j}" | sed -E 's/<.*|>.*|=.*//')"
+      declare current_array+=("${current_program}")
+    done
+      
+    create_array "${i}_no_relations" "${current_array[@]}"
+    unset current_array
+  done
   
   mapfile -t missing_dependencies < <(echo -n "${missing_dependencies}")
   mapfile -t missing_build_dependencies < <(echo -n "${missing_build_dependencies}")
