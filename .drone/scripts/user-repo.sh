1f55203c1fb5c715e7527740e965347df833c27c
Leo Puvilland <lpuvilla0001@mymail.lausd.net>
2021-11-10 14:37:06 -0800

    use @hwittenborn's solution

--- .drone/scripts/user-repo.sh
+++ .drone/scripts/user-repo.sh
@@ -49,6 +49,12 @@ if [[ "${target_repo}" == "aur" ]]; then
 	cat .SRCINFO
 fi
 
+# Check if changes have been made
+if [[ "$(git diff)" == "" ]]; then
+  exit 0
+fi
+
+
 # Set up Git identity information
 git config user.name "Kavplex Bot"
 git config user.email "kavplex@hunterwittenborn.com"
@@ -60,6 +66,6 @@ pkgrel="$(echo "${config}" | jq -r '.current_pkgrel')"
 
 # Commit changes and push
 git add PKGBUILD .SRCINFO
-git commit -m "Updated version to ${pkgver}-${pkgrel}" || true
+git commit -m "Updated version to ${pkgver}-${pkgrel}"
 
-git push || true
+git push
