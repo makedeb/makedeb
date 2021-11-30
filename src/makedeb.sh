0f12884b6f33758fda47a8e75a9bdcdf1900aa3a
Sam <39468367+Clos3y@users.noreply.github.com>
2021-11-30 15:32:13 +0000

    Update makedeb.sh
    Swapped `apt-get` for `apt`

--- src/makedeb.sh
+++ src/makedeb.sh
@@ -170,7 +170,7 @@ if [[ "${target_os}" == "debian" ]] && [[ ${INSTALL} == "TRUE" ]]; then
 
   apt_installation_string="$(echo "${pkgname[@]}" | sed 's| |, |g')"
   msg "Installing ${apt_installation_string}..."
-  sudo apt-get reinstall "${apt_args[@]}" -- "${apt_installation_list[@]}"
+  sudo apt reinstall "${apt_args[@]}" -- "${apt_installation_list[@]}"
 
   if (( "${makedeb_args["as-deps"]}" )); then
     sudo apt-mark auto -- "${pkgname[@]}" 1> /dev/null
