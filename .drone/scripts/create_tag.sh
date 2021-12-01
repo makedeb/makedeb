95301c91f949f4de3f5b5b6d3f74c317acaaea30
Leo Puvilland <leo@craftcat.dev>
2021-12-01 10:52:51 -0800

    ugh, finally fix dch command

--- .drone/scripts/create_tag.sh
+++ .drone/scripts/create_tag.sh
@@ -27,11 +27,15 @@ version="$(cat .data.json | jq -r '.current_pkgver + "-" + .current_pkgrel')"
 # Update debian version
 DEBVERSION="$(cat debian/changelog | cut -f2 -d" " - | grep "(" | cut -f2 -d"(" | cut -f1 -d")")"
 
+# Setup env vars needed
+DEBFULLNAME="Leo Puvilland"
+DEBEMAIL="leo@craftcat.dev"
+
 rm debian/changelog
 touch debian/changelog
 
 if [ $version != $DEBVERSION ]; then
-  dch --distribution unstable -v $version -i "Initial release (Closes: #998039)."
+  dch -D unstable -v $version "Initial release (Closes: #998039)."
 fi
 
 # Create and push release
