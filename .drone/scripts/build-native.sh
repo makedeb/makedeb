a2ffdc2e12c4a4d8526245f77468e5152ab19d45
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-11-30 22:24:18 -0600

    Added quotes around variable usage in native Debian packaging script in CI

--- .drone/scripts/build-native.sh
+++ .drone/scripts/build-native.sh
@@ -3,5 +3,5 @@ set -e
 
 git fetch
 export VERSION="$(cat .data.json | jq -r '.current_pkgver')"
-tar -cJf ../makedeb_$VERSION.orig.tar.xz .
+tar -cJf "../makedeb_$VERSION.orig.tar.xz" .
 debuild -us -uc
