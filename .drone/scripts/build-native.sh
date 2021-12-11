e56f74a3c11838134e412dee3ad02fa045eaf776
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-12-10 23:45:36 -0600

    Fixed archive name used by debuild in CI build stage; Added needed conflicting packages to build packages

--- .drone/scripts/build-native.sh
+++ .drone/scripts/build-native.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+stable_conflicts=('makedeb-beta' 'makedeb-alpha')
+beta_conflicts=('makedeb' 'makedeb-alpha')
+alpha_conflicts=('makedeb' 'makedeb-beta')
+conflicts="${release_type}_conflicts[@]"
+conflicts="${!conflicts}"
 
-# Set package name
+# Set package information.
 sed -i "s|\$\${pkgname}|${pkgname}|" debian/control
+sed -i "s|\$\${conflicts}|${conflicts}|" debian/control
 
+# Build package.
 git fetch
 export NEEDED_VERSION="$(cat .data.json | jq -r '.current_pkgver')"
-tar -cJf "../${pkgname}_${NEEDED_VERSION}.orig.tar.xz" .
+tar -cJf "../makedeb_${NEEDED_VERSION}.orig.tar.xz" .
 debuild -us -uc
 
-cp "../${pkgname}_${NEEDED_VERSION}-1_all.deb" ./
+cp "../makedeb_${NEEDED_VERSION}-1_all.deb" ./
