57927d01ece91c31e67e1a14b5252bbd34cb71f1
Leo Puvilland <leo@craftcat.dev>
2021-12-10 20:01:53 -0800

    fix requested changes

--- .drone/scripts/build-native.sh
+++ .drone/scripts/build-native.sh
@@ -1,24 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# Get current package version
-version="$(cat .data.json | jq -r '.current_pkgver + "-" + .current_pkgrel')"
-
-# Update debian version
-DEBVERSION="$(cat debian/changelog | cut -f2 -d" " - | grep "(" | cut -f2 -d"(" | cut -f1 -d")")"
-
-# Setup env vars needed
-DEBFULLNAME="Leo Puvilland"
-DEBEMAIL="leo@craftcat.dev"
-
-rm debian/changelog
-
-if [ $version != $DEBVERSION ]; then
-  dch --create --package makedeb -D unstable -v $version "Initial release (Closes: #998039)."
-fi
 
 # Set package name
-sed -i "s|\$\${pkgname}|${pkgname}| debian/control
+sed -i "s|\$\${pkgname}|${pkgname}|" debian/control
 
 git fetch
 export NEEDED_VERSION="$(cat .data.json | jq -r '.current_pkgver')"
