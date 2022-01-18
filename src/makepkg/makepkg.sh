0e091b952c13a787ce2ef259da0c4ba7d762ec1e
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-01-17 18:59:13 -0600

    Fixed missing 'pkgbase' variable for errors during PKGBUILD linting checks

--- src/makepkg/makepkg.sh
+++ src/makepkg/makepkg.sh
@@ -1306,10 +1306,12 @@ else
 	source_buildfile "$BUILDFILE"
 fi
 
+_pkgbase="${pkgbase:-${pkgname[0]}}"
+
 # check the PKGBUILD for some basic requirements
 lint_pkgbuild || exit $E_PKGBUILD_ERROR
 
-pkgbase=${pkgbase:-${pkgname[0]}}
+pkgbase="${_pkgbase}"
 
 # Exit regardless of sucess status if '--lint' was passed.
 (( "${LINT_PKGBUILD}" )) && exit 0
