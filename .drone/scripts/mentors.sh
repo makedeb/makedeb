2e423246f1430794236e33276b284ff42d93593b
Leo Puvilland <leo@craftcat.dev>
2022-01-26 17:29:57 -0800

    Switch to drone detecting branch

--- .drone/scripts/mentors.sh
+++ .drone/scripts/mentors.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ "${DRONE_COMMIT_BRANCH}" != "stable" ]]; then
-  echo "Not running stable branch. Abort."
-  exit 0
-fi
-
 cp files/dput.cf $HOME/.dput.cf
 
 export NEEDED_VERSION="$(cat .data.json | jq -r '.current_pkgver')"
