fac656e4dd815407eb2a1c3b964401d297ac28d1
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-02-03 22:30:49 -0600

    Added script to auto-publish releases on pushes to 'stable' branch
    Also moved Git tag creator to Python script (both the release publisher and Git tag creator are one script).
    
    Closes #98.

---
+++ .drone/scripts/create_tag.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import json
+import subprocess
+
+from os import environ
+from github import Github
+from github.GithubException import BadCredentialsException
+from github.Repository import Repository
+
+# Get all the info we need.
+github_api_key = environ["github_api_key"]
+drone_branch = environ["DRONE_COMMIT_BRANCH"]
+drone_repo = "makedeb/makedeb"
+commit_sha = subprocess.run(["git", "log", "-n", "1", "--format=%H"], stdout=subprocess.PIPE).stdout.decode().strip("\n")
+
+with open(".data.json", "r") as file:
+    data = json.loads(file.read())
+
+pkgver = data["current_pkgver"]
+pkgrel = data["current_pkgrel"]
+
+client = Github(github_api_key)
+
+tag = f"v{pkgver}-{pkgrel}-{drone_branch}"
+name = f"v{pkgver}-{pkgrel}"
+message = f"Released {name}."
+
+repo = client.get_repo(drone_repo)
+
+# We publish GitHub releases on pushes to the 'stable' branch. Otherwise, we just publish tags.
+if drone_branch == "stable":
+    repo.create_git_release(tag, name, message)
+else:
+    repo.create_git_tag(tag, "test", commit_sha, "commit")
+    repo.create_git_ref(f"refs/tags/{tag}", commit_sha)
