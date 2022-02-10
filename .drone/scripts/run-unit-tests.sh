597e7adbee94a25a60f7aea4a96e03750e666d45
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-02-09 18:35:19 -0600

    Removed leftover remnants of Docker setup

--- .drone/scripts/run-unit-tests.sh
+++ .drone/scripts/run-unit-tests.sh
@@ -2,14 +2,7 @@
 set -ex
 
 cd test/
-
 bats prepare/
-
-# Arguments may be passed to be this script to selectively run certain tests, such as when running from the local Docker Compose setup.
-if [[ -z "${@}" ]]; then
-    bats tests/
-else
-    bats "${@}"
-fi
+bats tests/
 
 # vim: set syntax=bash ts=4 sw=4 expandtab:
