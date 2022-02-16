a57d09f463c3c6314e72598a2495ee04986bfd9b
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-02-16 17:44:55 -0600

    Fixed issue with APT dependency checker not respecting provided packages (#119)
    Some other changes were also made in this commit:
    - Moved back to using 'apt-get satisfy' for dependency resolution, as it makes everything more reliable.
    - Moved indentation on some files to four spaces.
    - Added some Vim modelines.
    - Added some unit test utilities.
    
    Closes #87.

--- src/makedeb/functions/dependency_checks/check_missing_dependencies.sh
+++ src/makedeb/functions/dependency_checks/check_missing_dependencies.sh
@@ -1,50 +1,52 @@
 check_missing_dependencies() {
-  # Get list of missing dependencies.
-  if [[ "${target_os}" == "debian" ]]; then
-    if ! missing_dependencies="$("${makedeb_utils_dir}/missing_apt_dependencies.py" "${predepends[@]}" "${depends[@]}")"; then
-      error "There was an error checking build dependencies."
-      exit 1
-    elif ! missing_build_dependencies="$("${makedeb_utils_dir}/missing_apt_dependencies.py"  "${makedepends[@]}" "${checkdepends[@]}")"; then
-      error "There was an error checking build dependencies."
-      exit 1
-    fi
-        
-  elif [[ "${target_os}" == "arch" ]]; then
-    missing_dependencies="$(pacman -T -- "${predepends[@]}" "${depends[@]}")" || true
-    missing_build_dependencies="$(pacman -T -- "${makedepends[@]}" "${checkdepends[@]}")" || true
-  fi
-  
-  # Unset variables if no missing dependencies were found.
-  for i in 'missing_dependencies' 'missing_build_dependencies'; do
-    if [[ "${!i}" == "" ]]; then
-      unset "${i}"
-    fi
-  done
-    
-  # Map output of commands to arrays.
-  mapfile -t missing_dependencies < <(echo -n "${missing_dependencies}")
-  mapfile -t missing_build_dependencies < <(echo -n "${missing_build_dependencies}")
+    # Get list of missing dependencies.
+    if [[ "${target_os}" == "debian" ]]; then
+        if ! missing_dependencies="$("${makedeb_utils_dir}/missing_apt_dependencies.py" "${predepends[@]}" "${depends[@]}")"; then
+            error "There was an error checking build dependencies."
+            exit 1
+        elif ! missing_build_dependencies="$("${makedeb_utils_dir}/missing_apt_dependencies.py"  "${makedepends[@]}" "${checkdepends[@]}")"; then
+            error "There was an error checking build dependencies."
+            exit 1
+        fi
 
-  # Create dependency arrays without relationships attached (for when removing
-  # build dependencies).
-  declare missing_dependencies_no_relations=()
-  declare missing_build_dependencies_no_relations=()
-    
-  for i in 'missing_dependencies' 'missing_build_dependencies'; do
-    declare current_array=()
-    declare var_string="${i}[@]"
-            
-    for j in "${!var_string}"; do
-      declare current_program="$(echo "${j}" | sed -E 's/<.*|>.*|=.*//')"
-      declare current_array+=("${current_program}")
+    elif [[ "${target_os}" == "arch" ]]; then
+        missing_dependencies="$(pacman -T -- "${predepends[@]}" "${depends[@]}")" || true
+        missing_build_dependencies="$(pacman -T -- "${makedepends[@]}" "${checkdepends[@]}")" || true
+    fi
+
+    # Unset variables if no missing dependencies were found.
+    for i in 'missing_dependencies' 'missing_build_dependencies'; do
+        if [[ "${!i}" == "" ]]; then
+            unset "${i}"
+        fi
     done
-      
-    create_array "${i}_no_relations" "${current_array[@]}"
-    unset current_array
-  done
-      
-  declare -g missing_dependencies
-  declare -g missing_dependencies_no_relations=("${missing_dependencies_no_relations[@]}")
-  declare -g missing_build_dependencies
-  declare -g missing_build_dependencies_no_relations=("${missing_build_dependencies_no_relations[@]}")
+
+    # Map output of commands to arrays.
+    mapfile -t missing_dependencies < <(echo -n "${missing_dependencies}")
+    mapfile -t missing_build_dependencies < <(echo -n "${missing_build_dependencies}")
+
+    # Create dependency arrays without relationships attached (for when removing
+    # build dependencies).
+    declare missing_dependencies_no_relations=()
+    declare missing_build_dependencies_no_relations=()
+
+    for i in 'missing_dependencies' 'missing_build_dependencies'; do
+        declare current_array=()
+        declare var_string="${i}[@]"
+
+        for j in "${!var_string}"; do
+            declare current_program="$(echo "${j}" | grep -o '^[^ ]*')"
+            declare current_array+=("${current_program}")
+        done
+
+        create_array "${i}_no_relations" "${current_array[@]}"
+        unset current_array
+    done
+    
+    declare -g missing_dependencies
+    declare -g missing_build_dependencies
+    declare -g missing_dependencies_no_relations
+    declare -g missing_build_dependencies_no_relations
 }
+
+# vim: set syntax=bash ts=4 sw=4 expandtab:
--- src/makedeb/functions/dependency_checks/remove_installed_dependencies.sh
+++ src/makedeb/functions/dependency_checks/remove_installed_dependencies.sh
@@ -1,21 +1,24 @@
 remove_installed_dependencies() {
-  if [[ "${#missing_build_dependencies_no_relations[@]}" == "0" ]]; then
-    return 0
-  fi
-  
-  msg "Removing installed build dependencies..."
-  
-  if [[ "${target_os}" == "debian" ]]; then
-    if ! sudo apt-get purge "${apt_args[@]}" -- "${missing_build_dependencies_no_relations[@]}"; then
-      error "There was an error removing build dependencies."
-      exit 1
+    if [[ "${#missing_build_dependencies_no_relations[@]}" == "0" ]]; then
+        return 0
     fi
-  
-  elif [[ "${target_os}" == "arch" ]]; then
-    if ! sudo pacman -R "${pacman_args[@]}" -- "${missing_build_dependencies_no_relations[@]}"; then
-      error "There was an error removing build dependencies."
-      exit 1
+
+    msg "Removing installed build dependencies..."
+
+    if [[ "${target_os}" == "debian" ]]; then
+        if ! sudo apt-get purge "${apt_args[@]}" -- "${missing_build_dependencies_no_relations[@]}"; then
+            error "There was an error removing build dependencies."
+            exit 1
+        fi
+
+    elif [[ "${target_os}" == "arch" ]]; then
+        if ! sudo pacman -R "${pacman_args[@]}" -- "${missing_build_dependencies_no_relations[@]}"; then
+            error "There was an error removing build dependencies."
+            exit 1
+        fi
     fi
-  fi
-  msg "Done."
+
+    msg "Done."
 }
+
+# vim: set syntax=bash ts=4 sw=4 expandtab:
