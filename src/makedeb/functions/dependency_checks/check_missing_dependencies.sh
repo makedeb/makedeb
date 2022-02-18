c5745013f8fe58dd4479e5c93bb912f4800f9d2d
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-02-17 18:50:47 -0600

    Fixed issue with '-r' flag not working
    Looks like we need to initialize our arrays as global before working with them for Bash to act upon the global scope of them.

--- src/makedeb/functions/dependency_checks/check_missing_dependencies.sh
+++ src/makedeb/functions/dependency_checks/check_missing_dependencies.sh
@@ -1,34 +1,28 @@
 check_missing_dependencies() {
+    declare -g missing_dependencies=() \
+               missing_build_dependencies=() \
+               missing_dependencies_no_relations=() \
+               missing_build_dependencies_no_relations=()
+
     # Get list of missing dependencies.
     if [[ "${target_os}" == "debian" ]]; then
-        if ! missing_dependencies="$("${makedeb_utils_dir}/missing_apt_dependencies.py" "${predepends[@]}" "${depends[@]}")"; then
+        if ! mapfile -t missing_dependencies < <("${makedeb_utils_dir}/missing_apt_dependencies.py" "${predepends[@]}" "${depends[@]}"); then
             error "There was an error checking build dependencies."
             exit 1
-        elif ! missing_build_dependencies="$("${makedeb_utils_dir}/missing_apt_dependencies.py"  "${makedepends[@]}" "${checkdepends[@]}")"; then
+        elif ! mapfile -t missing_build_dependencies < <("${makedeb_utils_dir}/missing_apt_dependencies.py"  "${makedepends[@]}" "${checkdepends[@]}"); then
             error "There was an error checking build dependencies."
             exit 1
         fi
 
     elif [[ "${target_os}" == "arch" ]]; then
-        missing_dependencies="$(pacman -T -- "${predepends[@]}" "${depends[@]}")" || true
-        missing_build_dependencies="$(pacman -T -- "${makedepends[@]}" "${checkdepends[@]}")" || true
+        mapfile -t missing_dependencies < <(pacman -T -- "${predepends[@]}" "${depends[@]}") || true
+        mapfile -t missing_build_dependencies < <(pacman -T -- "${makedepends[@]}" "${checkdepends[@]}") || true
     fi
-
-    # Unset variables if no missing dependencies were found.
-    for i in 'missing_dependencies' 'missing_build_dependencies'; do
-        if [[ "${!i}" == "" ]]; then
-            unset "${i}"
-        fi
-    done
-
-    # Map output of commands to arrays.
-    mapfile -t missing_dependencies < <(echo -n "${missing_dependencies}")
-    mapfile -t missing_build_dependencies < <(echo -n "${missing_build_dependencies}")
 
     # Create dependency arrays without relationships attached (for when removing
     # build dependencies).
-    declare missing_dependencies_no_relations=()
-    declare missing_build_dependencies_no_relations=()
+    declare -g missing_dependencies_no_relations=()
+    declare -g missing_build_dependencies_no_relations=()
 
     for i in 'missing_dependencies' 'missing_build_dependencies'; do
         declare current_array=()
@@ -42,11 +36,6 @@ check_missing_dependencies() {
         create_array "${i}_no_relations" "${current_array[@]}"
         unset current_array
     done
-    
-    declare -g missing_dependencies
-    declare -g missing_build_dependencies
-    declare -g missing_dependencies_no_relations
-    declare -g missing_build_dependencies_no_relations
 }
 
 # vim: set syntax=bash ts=4 sw=4 expandtab:
