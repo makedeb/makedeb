92f3fa0d8b94f5a42d9a8da077408d30fa8ab19b
hiddeninthesand <hiddeninthesand@protonmail.com>
2022-03-03 13:59:29 -1000

    Use ZSTD by default

--- src/makedeb/functions/build_deb.sh
+++ src/makedeb/functions/build_deb.sh
@@ -1,34 +1,37 @@
 # This function should be run from the directory containing the 'DEBIAN' folder.
+zstdthreads=0
+zstdlevel=10
 build_deb() {
   local pkgname="${1}" \
         tar_control_arguments \
         tar_data_arguments
 
-  # Generate control.tar.gz archive.
+  # Generate control.tar.zst archive.
   cd DEBIAN/
   mapfile -t tar_control_arguments < <(find ./ | grep -v '^\./$')
-  tar -czf ./control.tar.gz "${tar_control_arguments[@]}"
+  tar -cf ./control.tar "${tar_control_arguments[@]}"
+  zstd -T${zstdthreads} -${zstdlevel} --rm -q control.tar
   cd ..
 
-  # Generate data.tar.gz archive.
+  # Generate data.tar.zst archive.
   mapfile -t tar_data_arguments < <(find ./ -maxdepth 1 | grep -v '^\./$' | grep -v '^\./DEBIAN$')
 
   # Tar will freak out if we don't supply any directories here (which will
   # happen when no items were created in ${pkgdir}), so we create the file
   # with no data when such a scenario arrises.
   if [[ "${tar_data_arguments}" != "" ]]; then
-    tar -czf data.tar.gz "${tar_data_arguments[@]}"
+    tar -cf data.tar "${tar_data_arguments[@]}"
   else
-    printf '' | tar -czf data.tar.gz --files-from -
+    printf '' | tar -cf data.tar --files-from -
   fi
-
+  zstd -T${zstdthreads} -${zstdlevel} --rm -q data.tar
   # Create the debian-binary file.
   echo "2.0" > debian-binary
 
-  # Move control.tar.gz from DEBIAN/ to pkgdir.
-  mv ./DEBIAN/control.tar.gz ./control.tar.gz
+  # Move control.tar.zst from DEBIAN/ to pkgdir.
+  mv ./DEBIAN/control.tar.zst ./control.tar.zst
 
   # Create the .deb package, and remove extra files we created.
-  ar -rU "${pkgname}_${pkgver}_${MAKEDEB_CARCH}.deb" debian-binary control.tar.gz data.tar.gz &> /dev/null
-  rm debian-binary control.tar.gz data.tar.gz
+  ar -rU "${pkgname}_${pkgver}_${MAKEDEB_CARCH}.deb" debian-binary control.tar.zst data.tar.zst &> /dev/null
+  rm debian-binary control.tar.zst data.tar.zst
 }
