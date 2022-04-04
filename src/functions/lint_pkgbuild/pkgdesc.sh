716f2db087492fd50801cecdca4779f789dbf123
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-04-04 15:32:16 -0500

    Check if pkgdesc variable was set

--- src/functions/lint_pkgbuild/pkgdesc.sh
+++ src/functions/lint_pkgbuild/pkgdesc.sh
@@ -1,13 +1,21 @@
 lint_pkgbuild_functions+=('lint_pkgdesc')
 
 lint_pkgdesc() {
-	if [[ "${pkgdesc}" == "" ]]; then
-		error "$(gettext "pkgdesc cannot be empty.")"
-		return 1
-	fi
+    # See if the pkgdesc variable was set.
+    if [[ "${pkgdesc:+x}" != "x" ]]; then
+	return 0
+    fi
+    
+    # Check pkgdesc requirements.
+    if [[ "${pkgdesc}" == "" ]]; then
+	error "$(gettext "pkgdesc cannot be empty.")"
+	return 1
+    fi
 
-	if echo "${pkgdesc}" | grep -q '^[ ]*$'; then
-		error "$(gettext "pkgdesc must contain characters other than spaces.")"
-		return 1
-	fi
+    if echo "${pkgdesc}" | grep -q '^[ ]*$'; then
+	error "$(gettext "pkgdesc must contain characters other than spaces.")"
+	return 1
+    fi
 }
+
+# vim: set sw=4 expandtab:
