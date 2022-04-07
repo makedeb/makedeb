16d385e8f989f2a746f6b9b72a32dde14511eabb
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-04-07 18:46:27 -0500

    Various control field and SRCINFO fixes/changes
    - Fixed an issue where makedeb would report duplicate control fields being present when 'control_fields' was specified in a PKGBUILD.
    - Redid SRCINFO generator. No tabs are present now, and we now write all distro-specific values.
    - Fixed an issue where user-specified control fields didn't get written to control files.

--- src/functions/lint_pkgbuild/control_fields.sh
+++ src/functions/lint_pkgbuild/control_fields.sh
@@ -6,12 +6,13 @@ lint_control_fields() {
 	local current_control_field
 	local control_field_name
 	local control_field_value
+        declare -g MERGED_CONTROL_FIELDS=("${CONTROL_FIELDS[@]}")
 
 	for control_field in "${control_fields[@]}"; do
-		CONTROL_FIELDS+=("${control_field}")
+		MERGED_CONTROL_FIELDS+=("${control_field}")
 	done
 
-	for control_field in "${CONTROL_FIELDS[@]}"; do
+	for control_field in "${MERGED_CONTROL_FIELDS[@]}"; do
 		mapfile -t current_control_field < <(echo "${control_field}")
 
 		if [[ "${#current_control_field[@]}" != 1 ]]; then
@@ -26,13 +27,13 @@ lint_control_fields() {
 		matches="$(occurances_in_array "${control_field_name}" "${control_field_names[@]}")"
 
 		if [[ "${matches}" != 0 ]]; then
-			[[ "${matches}" == 1 ]] && error "$(gettext "Control field %s was found more than once.")" "${control_field_name}"
+			[[ "${matches}" == 1 ]] && error "$(gettext "Control field '%s' was found more than once.")" "${control_field_name}"
 			ret=1
 		fi
 
 		control_field_names+=("${control_field_name}")
 	done
-
+        
 	return "${ret}"
 }
 
