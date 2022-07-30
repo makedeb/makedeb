b99a7f25b8299048002b3c221a48fb844722974c
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-07-30 02:46:15 -0500

    Add Patreon sponsors to release notes

--- .drone/scripts/create_tag.sh
+++ .drone/scripts/create_tag.sh
@@ -13,18 +13,28 @@ echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https
 apt update
 apt-get install gh parse-changelog -y
 
-# If the release is stable, get release notes and make a new production release. Otherwise, add no release notes and make a prerelease.
 branch="${DRONE_COMMIT_BRANCH}"
 pkgver="$(cat .data.json | jq -r '.current_pkgver')"
 pkgrel="$(cat .data.json | jq -r ".current_pkgrel_${branch}")"
 
 echo "${github_api_key}" | gh auth login --with-token
 
-if [[ "${DRONE_COMMIT_BRANCH}" == 'stable' ]]; then
-    release_notes="$(parse-changelog CHANGELOG.md "${pkgver}")"
-    gh release create "v${pkgver}-${pkgrel}" --title "v${pkgver}-${pkgrel}" --target "${DRONE_COMMIT_SHA}" -n "${release_notes}"
+# Generate release notes and create release.
+release_notes=$'## Changes\n'
+gh_cli_args=()
+
+if [[ "${branch}" == 'stable' ]]; then
+    release_notes+="$(parse-changelog CHANGELOG.md "${pkgver}")"
 else
-    gh release create "v${pkgver}-${pkgrel}" --title "v${pkgver}-${pkgrel}" --target "${DRONE_COMMIT_SHA}" -n "This is a **prerelease**\! Things may be broken, and you should avoid this release if you're expecting stability." --prerelease
+    release_notes+=$'> The following contains the list of changes since the last stable release.\n\n'
+    release_notes+="$(parse-changelog CHANGELOG.md 'Unreleased')"
+    gh_cli_args+=('--prerelease')
 fi
+
+release_notes+=$'\n\n## Supporters\nThank you to the following people for helping to make this release possible:\n'
+release_notes+="$(cat patreon-sponsors.txt | sed 's|.*|- &|')"
+
+# If the release is stable, make a new production release. Otherwise, make a prerelease.
+gh release create "v${pkgver}-${pkgrel}" --title "v${pkgver}-${pkgrel}" --target "${DRONE_COMMIT_SHA}" -n "${release_notes}" "${gh_cli_args[@]}"
 
 # vim: set sw=4 expandtab:
