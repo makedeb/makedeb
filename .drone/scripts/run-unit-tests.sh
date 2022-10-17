290e876738b4a79de445eea3940a56f49538e189
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-10-17 01:34:45 -0500

    pushing

--- .drone/scripts/run-unit-tests.sh
+++ .drone/scripts/run-unit-tests.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/bash
 set -e
 
-# REMOVE BEFORE MERGING!!!
-echo "${PATH}" &> /dev/stderr
-
 if (git log -1 --pretty='%s' | grep '\[TEST SKIP\]$') || ([[ "${TEST_SKIP:+x}" == 'x' ]]); then
     echo "Skipping unit tests..."
     exit 0
