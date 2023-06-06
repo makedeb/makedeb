dbe913d7d4195be79d4993d84972c3f63757dd5f
bunnylo1 <bunnylo12@yahoo.com>
2023-06-06 12:46:48 +0300

    update

--- src/functions/binary/apt_satisfy.py
+++ src/functions/binary/apt_satisfy.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 import subprocess as sbp
-from os import system
 import sys
 def run(*a):
 	return sbp.run(a, stdout=subprocess.PIPE).stdout.decode('utf-8')
+def system(*a):
+	return sbp.call(a)
 
 def runquery():
 	return set(run("dpkg-query", "-Wf", "${Package}\\n").split("\n"))
 
 prev_installed_packages = runquery()
 
-if (system("apt-get satisfy " + ' '.join(sys.argv) != 0): #{
+if (system("apt-get", "satisfy", *sys.argv) != 0): #{
 #error "$(gettext "Failed to install missing dependencies.")"
     exit(1);
 
@@ -19,7 +20,7 @@ cur_installed_packages = runquery()
 
 newly_installed_packages = cur_installed_packages.difference(prev_installed_packages)
 if (len(newly_installed_packages) > 0):
-     if (system("apt-mark -qqqq auto " + " ".join(newly_installed_packages)) != 0):
+     if (system("apt-mark", "-qqqq",  "auto", *newly_installed_packages) != 0):
         #error "$(gettext "Failed to install missing dependencies.")"
           print "some strange error";
           exit 1;
